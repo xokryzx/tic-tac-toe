6bb46499357d906ca1d6b0e296a6ed654de38fb4
xokryzx <xokryzx@gmail.com>
2023-09-10 01:20:52 +0900

    ✨ Add a function to alternate between marking X and O according to the order

--- src/App.js
+++ src/App.js
@@ -10,11 +10,16 @@ function Square({ value, onSquareClick }) {
 }
 
 export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
+    if (squares[i]) return;
+
     const nextSquares = squares.slice();
-    nextSquares[i] = "X";
+    xIsNext ? (nextSquares[i] = "X") : (nextSquares[i] = "O");
+
+    setXIsNext(!xIsNext);
     setSquares(nextSquares);
   }
 
